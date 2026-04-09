978be499f74aa50a99caac1a4e5de2332e60ae22
user_name <example@example.com>
2026-04-09 15:05:36 +0000

    fix: try to resolve ts errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/gufa-hanhua/',
   resolve: {
     alias: {
       '@': '/src'
